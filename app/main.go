30155ca9e7d543df0481aae1f9c5c3a8abefe62a
Pranav <pranav.prakash@causaly.com>
2025-03-06 00:32:52 +0000

    playing around for

--- app/main.go
+++ app/main.go
@@ -13,11 +13,13 @@ var _ = fmt.Fprint
 func main() {
 	// Uncomment this block to pass the first stage
 	// Wait for user input
-	fmt.Fprint(os.Stdout, "$ ")
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	fmt.Printf("%s: command not found\n", strings.TrimSpace(input))
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		fmt.Printf("%s: command not found\n", strings.TrimSpace(input))
 
-	if err != nil {
-		fmt.Printf("%s: invalid input\n", input)
+		if err != nil {
+			fmt.Printf("%s: invalid input\n", input)
+		}
 	}
 }
