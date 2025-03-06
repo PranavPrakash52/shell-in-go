6542571516baa082befc700f50a00e84c40495ba
Pranav <pranav.prakash@causaly.com>
2025-03-06 00:23:59 +0000

    playing around stdin

--- app/main.go
+++ app/main.go
@@ -11,8 +11,7 @@ var _ = fmt.Fprint
 
 func main() {
 	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Fprint(os.Stdout, "invalid_command: command not found")
 }
