4b18ab8b3d253219790ef84f81ba086cb889d211
Pranav <pranav.prakash@causaly.com>
2025-03-09 16:43:25 +0000

    added support for custom commands

--- app/main.go
+++ app/main.go
@@ -64,10 +64,9 @@ func main() {
 				fmt.Printf("%s: not found\n", words[1])
 			}
 		} else if _, ok := map_[command]; ok {
-			cmd_ := exec.Command(map_[command], words[1:]...)
+			cmd_ := exec.Command(map_[command] + "/" + command, words[1:]...)
 			err := cmd_.Run()
 			if err != nil {
-				fmt.Println(err)
 				fmt.Printf("%s: command not found\n", command)
 			}
 		} else {
